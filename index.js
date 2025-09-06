09a39d6070656e85646766f4e74aa061e03b2338
Kai Pereira <kaipereira2020@gmail.com>
2025-09-05 21:31:15 -0700

    adding the boilerplate

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { WebClient } from "@slack/web-api";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const client = new WebClient(process.env.SLACK_BOT_TOKEN);
+const PRIVATE_CHANNEL_ID = "C1234567890";
+
+app.post("/join", async (req, res) => {
+	const userId = req.body.user_id;
+
+	try {
+		await client.conversations.invite({
+			channel: PRIVATE_CHANNEL_ID,
+			users: userId,
+		});
+
+		res.send(`Is that really you <@${userId}>? Welcome to my mindspace :D`);
+	} catch (error) {
+		console.error(error);
+		res.send("Whoooops, my cat hit the self-destruct server button, or maybe you're just already in the channel?");
+	}
+});
+
+app.listen(3000, () => console.log("Kai's AI overlord is already running on port 3000"));
+
